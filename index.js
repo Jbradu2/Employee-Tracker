c6934dba3173e2eee0a1a4eb83b07cf2165982b4
Jbradu2 <138729161+Jbradu2@users.noreply.github.com>
2023-11-14 17:56:54 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 
 // MySQL connection
+// MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Slickhead11!',
-  database: 'employee_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 // Connect to the database
